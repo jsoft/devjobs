fbdd1384a752445a0d4f3d46c647cceff9090455
jsoft <jmiguel.gomez@gmail.com>
2023-03-02 13:09:34 -0500

    Agregando el campo de rol a la entidad usuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,23 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Rol -->
+        <div class="mt-4">
+            <x-input-label for="rol" :value="__('¿Que tipo de cuenta deseas en DevJobs?')" />
+
+            <select
+                id="rol"
+                name="rol"
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            >
+            <option value="">-- Selecciona un Rol --</option>
+            <option value="1"> Developer - Obtener empleo</option>
+            <option value="2"> Recruiter - Publicar empleos</option>
+            </select>
+
+            <x-input-error :messages="$errors->get('rol')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
